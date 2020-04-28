94da8f857016d38679f5b87e0b87d3f745cf1694
W-LoD <dinashoronin1@gmail.com>
2020-04-28 11:51:12 +0300

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,37 @@
+﻿#include <math.h> 
+#include<iostream> 
+using namespace std;
+float F1(float x)
+{
+	float result = 0;
+	if (x >= 0)
+	{
+		result = 5 * x + 7;
+	}
+	else
+	{
+		result = 1 / x;
+	}
+	return result;
+}
+void F2(float& x)
+{
+	x = F1(x);
+}
+int main()
+{
+	for (float i = -5; i < 3; i++)
+	{
+		float f = F1(i);
+		cout « "i=" « i « ";F1(i)=" « f « endl;
+	}
+	cout « endl;
+	for (float i = -5; i < 3; i++)
+	{
+		cout « "i=" « i « ",";
+		F2(i);
+		cout « "F2=" « i «endl;
+	}
+	system("pause");
+	return 0;
+}
